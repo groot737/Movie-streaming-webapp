89ffa32e5006c5a8e5d8e648dedc5cce9e33adc9
Groot737 <giorgiquchuloria7@gmail.com>
2026-01-13 23:58:16 +0400

    fixed lists not displayed on vercel

--- api/auth/[...].js
+++
@@ -1,7 +0,0 @@
-import { getAuthApp } from "../../server/authApp.js";
-
-const app = getAuthApp();
-
-export default function handler(req, res) {
-  return app(req, res);
-}
---
+++ api/index.js
@@ -0,0 +1,7 @@
+import { getAuthApp } from "../server/authApp.js";
+
+const app = getAuthApp();
+
+export default function handler(req, res) {
+    return app(req, res);
+}
